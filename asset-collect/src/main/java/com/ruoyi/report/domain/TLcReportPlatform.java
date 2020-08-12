cf35533a80f7f3fbf95821ecff9c33721d199973
liurunkai <liurunkai@chinadatagroup.com>
2020-08-12 15:30:16 +0800

    修改bug

--- asset-collect/src/main/java/com/ruoyi/report/domain/TLcReportPlatform.java
+++ asset-collect/src/main/java/com/ruoyi/report/domain/TLcReportPlatform.java
@@ -41,7 +41,7 @@ public class TLcReportPlatform extends BaseEntity {
     /**
      * 平安通话时长
      */
-    @Excel(name = "平安通话时长")
+    @Excel(name = "平安通话时长(分)")
     private String paCallLen;
 
     /**
@@ -59,7 +59,7 @@ public class TLcReportPlatform extends BaseEntity {
     /**
      * 自建通话时长
      */
-    @Excel(name = "自建通话时长")
+    @Excel(name = "自建通话时长(分)")
     private String zjCallLen;
 
     /**
@@ -77,7 +77,7 @@ public class TLcReportPlatform extends BaseEntity {
     /**
      * 全部通话时长
      */
-    @Excel(name = "全部通话时长")
+    @Excel(name = "全部通话时长(分)")
     private String totalCallLen;
 
     /**
