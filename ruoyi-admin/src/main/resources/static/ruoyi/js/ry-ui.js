a870ae1b86410c3365eae5e4f01deeca9d2f4939
gaohg <gaohg@smartdot.com>
2020-09-02 10:30:05 +0800

    我的任务列表字段调整后刷新页面字段不变（cookie做法）

--- ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
@@ -111,6 +111,7 @@ var table = {
                     onDblClickCell: options.onDblClickCell,             // 双击某格触发的事件
                     onEditableSave: options.onEditableSave,             // 行内编辑保存的事件
                     onExpandRow: options.onExpandRow,                   // 点击详细视图的事件
+					onColumnSwitch: options.onColumnSwitch,             // 当切换列的显示状态（可见或不可见）时触发
                     rememberSelected: options.rememberSelected,         // 启用翻页记住前面的选择
                     fixedColumns: options.fixedColumns,                 // 是否启用冻结列（左侧）
                     fixedNumber: options.fixedNumber,                   // 列冻结的个数（左侧）
